bf525ec41df2dc43fee0b20b61177f06dbcad8d2
Roeck <rons9n@gmail.com>
2020-02-05 23:34:36 -0500

    created database with mondodb atlas

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const { ApolloServer } = require('apollo-server')
 
-const typeDefs = require('./typeDefs')
-const resolvers = require('./resolvers')
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+    .connect(process.env.MONGO_URI, { useNewUrlParser: 
+     true })
+     .then(() => console.log("DB connected"))
+     .catch(err => console.log(err));
 
 const server = new ApolloServer({
     typeDefs,
