64d43ffb4bf1c6edb4fa9a4afaf0d95187ff252e
Roeck <rons9n@gmail.com>
2020-02-09 23:54:25 -0500

    authenticating users from apollo server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const { ApolloServer } = require('apollo-server')
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
-const mongoose = require("mongoose");
-require("dotenv").config();
+const { findOrCreateUser } = require('./controllers/userController')
+
 
 mongoose
     .connect(process.env.MONGO_URI, { useNewUrlParser: 
@@ -13,8 +15,21 @@ mongoose
 
 const server = new ApolloServer({
     typeDefs,
-    resolvers
-})
+    resolvers,
+    context: async ({ req }) => {
+        let authToken = null
+        let currentUser = null
+        try {
+            authToken = req.headers.authorization
+            if (authToken) {
+                currentUser = await findOrCreateUser(authToken)
+            }
+        } catch (err) {
+          console.error(`Unable to authenticate user with token ${authToken}`)
+        }
+        return { currentUser }
+    } 
+});
 
 server.listen().then(({ url }) => {
     console.log(`Server listening on ${url}`);
